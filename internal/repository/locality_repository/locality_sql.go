f7c850efffdd8436efb14064226eff7c7d182cd6
gumenendez_meli <guido.menendez@mercadolibre.com>
2025-07-13 19:15:57 -0300

    Add locality model + repo interface and impl

---
+++ internal/repository/locality_repository/locality_sql.go
@@ -0,0 +1,20 @@
+package locality_repository
+
+import (
+	"app/internal/repository/sql_utils"
+	"app/pkg/models"
+	"database/sql"
+)
+
+type LocalityRepositorySql struct {
+	db *sql.DB
+}
+
+func (r *LocalityRepositorySql) CreateLocality(locality models.Locality) (models.Locality, error) {
+	_, err := sql_utils.Insert(r.db, "INSERT INTO localities (id, locality_name, province_name, country_name) VALUES (?, ?, ?, ?)", []any{locality.Id, locality.LocalityName, locality.ProvinceName, locality.CountryName})
+	if err != nil {
+		return locality, err
+	}
+
+	return locality, nil
+}
