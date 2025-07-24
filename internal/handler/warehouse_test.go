0f0db91affe42af55d7fcee27634eb24cbb98099
lmagri <ludmila.magri@mercadolibre.com>
2025-07-24 15:06:14 -0300

    fix de tests create, finalizacion de tests en handler

--- internal/handler/warehouse_test.go
+++ internal/handler/warehouse_test.go
@@ -24,7 +24,7 @@ func Test_GetWarehouse(t *testing.T) {
 			{
 				Id:                 1,
 				WarehouseCode:      "Wh-1",
-				Address:            "Calle 1",
+				Address:            "Street 1",
 				Telephone:          "1234567890",
 				MinimumCapacity:    100,
 				MinimumTemperature: &[]float64{10}[0],
@@ -32,7 +32,7 @@ func Test_GetWarehouse(t *testing.T) {
 			{
 				Id:                 2,
 				WarehouseCode:      "Wh-2",
-				Address:            "Calle 2",
+				Address:            "Street 2",
 				Telephone:          "1494945693",
 				MinimumCapacity:    200,
 				MinimumTemperature: &[]float64{20}[0],
@@ -50,23 +50,13 @@ func Test_GetWarehouse(t *testing.T) {
 		require.Equal(t, http.StatusOK, response.Code)
 		require.Equal(t, "application/json", response.Header().Get("Content-Type"))
 
-		var res map[string]any
+		var res struct {
+			Data []models.Warehouse `json:"data"`
+		}
 		err := json.Unmarshal(response.Body.Bytes(), &res)
 		require.NoError(t, err)
 
-		require.Contains(t, res, "data")
-
-		data, ok := res["data"].([]any)
-		require.True(t, ok)
-		require.Len(t, data, 2)
-
-		firstWarehouse := data[0].(map[string]any)
-		require.Equal(t, float64(1), firstWarehouse["id"])
-		require.Equal(t, "Wh-1", firstWarehouse["warehouse_code"])
-		require.Equal(t, "Calle 1", firstWarehouse["address"])
-		require.Equal(t, "1234567890", firstWarehouse["telephone"])
-		require.Equal(t, float64(100), firstWarehouse["minimum_capacity"])
-		require.Equal(t, float64(10), firstWarehouse["minimum_temperature"])
+		require.Equal(t, expectedWarehouses, res.Data)
 
 		mockService.AssertExpectations(t)
 	})
@@ -90,7 +80,7 @@ func Test_GetWarehouse(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Contains(t, res, "message")
-		require.Contains(t, res["message"], "Warehouse not found")
+		require.Contains(t, res, "error")
 
 		mockService.AssertExpectations(t)
 	})
@@ -101,7 +91,7 @@ func Test_GetWarehouseById(t *testing.T) {
 		expectedWarehouse := models.Warehouse{
 			Id:                 1,
 			WarehouseCode:      "Wh-1",
-			Address:            "Calle 1",
+			Address:            "Street 1",
 			Telephone:          "1234567890",
 			MinimumCapacity:    100,
 			MinimumTemperature: &[]float64{10}[0],
@@ -221,7 +211,7 @@ func Test_CreateWarehouse(t *testing.T) {
 	t.Run("should create the warehouse successfully", func(t *testing.T) {
 		body := `{
 			"warehouse_code": "Wh-1",
-			"address": "Calle 1",
+			"address": "Street 1",
 			"telephone": "1234567890",
 			"minimum_capacity": 100,
 			"minimum_temperature": 10
@@ -229,7 +219,7 @@ func Test_CreateWarehouse(t *testing.T) {
 
 		input := models.Warehouse{
 			WarehouseCode:      "Wh-1",
-			Address:            "Calle 1",
+			Address:            "Street 1",
 			Telephone:          "1234567890",
 			MinimumCapacity:    100,
 			MinimumTemperature: &[]float64{10}[0],
@@ -287,7 +277,7 @@ func Test_CreateWarehouse(t *testing.T) {
 	t.Run("should handle bad request error for malformed body", func(t *testing.T) {
 		body := `{
 			"warehouse_code": "Wh-1",
-			"address": "Calle 1",
+			"address": "Street 1",
 			"telephone": "1234567890",
 			"minimum_capacity": 100,
 			"minimum_temperature": 10,
@@ -316,14 +306,14 @@ func Test_CreateWarehouse(t *testing.T) {
 	t.Run("should handle conflict error for unique constraint violation", func(t *testing.T) {
 		body := `{
 			"warehouse_code": "Wh-1",
-			"address": "Calle 1",
+			"address": "Street 1",
 			"telephone": "1234567890",
 			"minimum_capacity": 100,
 			"minimum_temperature": 10
 		}`
 		input := models.Warehouse{
 			WarehouseCode:      "Wh-1",
-			Address:            "Calle 1",
+			Address:            "Street 1",
 			Telephone:          "1234567890",
 			MinimumCapacity:    100,
 			MinimumTemperature: &[]float64{10}[0],
@@ -355,7 +345,7 @@ func Test_CreateWarehouse(t *testing.T) {
 	t.Run("should handle unprocessable entity error for missing required fields", func(t *testing.T) {
 		body := `{
 			"warehouse_code": "Wh-1",
-			"address": "Calle 1"
+			"address": "Street 1"
 		}`
 		mockService := new(service.MockWarehouseService)
 		hd := NewWarehouseDefault(mockService)
@@ -379,7 +369,7 @@ func Test_CreateWarehouse(t *testing.T) {
 	t.Run("should handle unprocessable entity error for invalid fields values", func(t *testing.T) {
 		body := `{
 			"warehouse_code": "Wh-1",
-			"address": "Calle 1",
+			"address": "Street 1",
 			"telephone": "",
 			"minimum_capacity": 100,
 			"minimum_temperature": 10
@@ -403,18 +393,47 @@ func Test_CreateWarehouse(t *testing.T) {
 		mockService.AssertNotCalled(t, "CreateWarehouse", 0)
 	})
 
+	t.Run("should return error if minimum_capacity is negative", func(t *testing.T) {
+		body := `{
+		"warehouse_code": "Wh-1",
+		"address": "Street 1",
+		"telephone": "1234567890",
+		"minimum_capacity": -1,
+		"minimum_temperature": 10
+	}`
+
+		mockService := new(service.MockWarehouseService)
+		hd := NewWarehouseDefault(mockService)
+
+		request := httptest.NewRequest(http.MethodPost, "/warehouses", bytes.NewBufferString(body))
+		request.Header.Set("Content-Type", "application/json")
+		response := httptest.NewRecorder()
+
+		hd.CreateWarehouse(response, request)
+
+		require.Equal(t, http.StatusUnprocessableEntity, response.Code)
+
+		var res map[string]any
+		err := json.Unmarshal(response.Body.Bytes(), &res)
+		require.NoError(t, err)
+		require.Contains(t, res, "message")
+		require.Contains(t, res, "error")
+
+		mockService.AssertNotCalled(t, "CreateWarehouse", 0)
+	})
+
 }
 
 func Test_PatchWarehouse(t *testing.T) {
 	t.Run("should update the warehouse successfully", func(t *testing.T) {
 		body := `{
 			"warehouse_code": "Wh-1",
-			"address": "Calle 1",
+			"address": "Street 1",
 			"telephone": "1234567890",
 			"minimum_capacity": 900,
 			"minimum_temperature": 10
 		}`
-		address := "Calle 1234"
+		address := "Street 1234"
 		minimumCapacity := 900
 
 		returned := models.Warehouse{
@@ -430,7 +449,7 @@ func Test_PatchWarehouse(t *testing.T) {
 		mockService.On("UpdateWarehouseById", 1, mock.MatchedBy(func(wh models.Warehouse) bool {
 			return wh.WarehouseCode == "Wh-1" &&
 				wh.Telephone == "1234567890" &&
-				wh.Address == "Calle 1" &&
+				wh.Address == "Street 1" &&
 				wh.MinimumCapacity == 900 &&
 				wh.MinimumTemperature != nil && *wh.MinimumTemperature == 10
 		})).Return(returned, nil)
@@ -543,12 +562,24 @@ func Test_PatchWarehouse(t *testing.T) {
 
 	t.Run("should handle not found error", func(t *testing.T) {
 		body := `{
-			"address": "Calle Falsa 123",
+			"warehouse_code": "1230",
+			"address": "Fake Street 123",
+			"telephone": "1234567890",
+			"minimum_capacity": 100,
+			"minimum_temperature": 10
 		}`
-		address := "Calle Falsa 123"
+		address := "Fake Street 123"
+		warehouseCode := "1230"
+		telephone := "1234567890"
+		minimumCapacity := 100
+		minimumTemperature := 10.0
 
 		inputPatch := models.Warehouse{
-			Address: address,
+			WarehouseCode:      warehouseCode,
+			Address:            address,
+			Telephone:          telephone,
+			MinimumCapacity:    minimumCapacity,
+			MinimumTemperature: &minimumTemperature,
 		}
 		mockService := new(service.MockWarehouseService)
 		mockService.On("UpdateWarehouseById", 1, inputPatch).Return(models.Warehouse{}, custom_errors.ErrNotFound)
@@ -605,7 +636,7 @@ func Test_PatchWarehouse(t *testing.T) {
 	t.Run("should handle unprocessable entity error for invalid fields values", func(t *testing.T) {
 		body := `{
 			"warehouse_code": "Wh-1",
-			"address": "Calle 1",
+			"address": "Street 1",
 			"telephone": "",
 			"minimum_capacity": 100,
 			"minimum_temperature": 10
@@ -631,5 +662,150 @@ func Test_PatchWarehouse(t *testing.T) {
 		require.Contains(t, res, "error")
 
 		mockService.AssertNotCalled(t, "UpdateWarehouseById", 0)
+	})
+}
+
+func Test_DeleteWarehouse(t *testing.T) {
+	t.Run("should delete the warehouse successfully", func(t *testing.T) {
+		mockService := new(service.MockWarehouseService)
+		mockService.On("DeleteWarehouse", 1).Return(nil)
+
+		hd := NewWarehouseDefault(mockService)
+
+		request := httptest.NewRequest(http.MethodDelete, "/warehouses/1", nil)
+		routeCtx := chi.NewRouteContext()
+		routeCtx.URLParams.Add("id", "1")
+		request = request.WithContext(context.WithValue(request.Context(), chi.RouteCtxKey, routeCtx))
+		response := httptest.NewRecorder()
+
+		hd.DeleteWarehouse(response, request)
+
+		require.Equal(t, http.StatusNoContent, response.Code)
+		require.Equal(t, "{}", response.Body.String())
+
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("should handle not found error", func(t *testing.T) {
+		mockService := new(service.MockWarehouseService)
+		mockService.On("DeleteWarehouse", 1).Return(custom_errors.ErrNotFound)
+		hd := NewWarehouseDefault(mockService)
+
+		request := httptest.NewRequest(http.MethodDelete, "/warehouses/1", nil)
+		routeCtx := chi.NewRouteContext()
+		routeCtx.URLParams.Add("id", "1")
+		request = request.WithContext(context.WithValue(request.Context(), chi.RouteCtxKey, routeCtx))
+		response := httptest.NewRecorder()
+
+		hd.DeleteWarehouse(response, request)
+
+		require.Equal(t, http.StatusNotFound, response.Code)
+		require.Equal(t, "application/json", response.Header().Get("Content-Type"))
+
+		var res map[string]any
+		err := json.Unmarshal(response.Body.Bytes(), &res)
+		require.NoError(t, err)
+		require.Contains(t, res, "message")
+		require.Contains(t, res, "error")
+
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("should handle bad request error for invalid url param", func(t *testing.T) {
+		mockService := new(service.MockWarehouseService)
+		hd := NewWarehouseDefault(mockService)
+
+		request := httptest.NewRequest(http.MethodDelete, "/warehouses/w", nil)
+		routeCtx := chi.NewRouteContext()
+		routeCtx.URLParams.Add("id", "w")
+		request = request.WithContext(context.WithValue(request.Context(), chi.RouteCtxKey, routeCtx))
+		response := httptest.NewRecorder()
+
+		hd.DeleteWarehouse(response, request)
+
+		require.Equal(t, http.StatusBadRequest, response.Code)
+		require.Equal(t, "application/json", response.Header().Get("Content-Type"))
+
+		var res map[string]any
+		err := json.Unmarshal(response.Body.Bytes(), &res)
+		require.NoError(t, err)
+		require.Contains(t, res, "message")
+		require.Contains(t, res, "error")
+		mockService.AssertNotCalled(t, "DeleteWarehouse", 0)
+	})
+}
+
+func Test_validateWarehouseAttributes(t *testing.T) {
+	minimumTemperature := 5.0
+
+	t.Run("should return error if warehouse_code is empty", func(t *testing.T) {
+		err := validateWarehouseAttributes(models.Warehouse{
+			WarehouseCode:      "",
+			Address:            "Street 1",
+			Telephone:          "1234567890",
+			MinimumCapacity:    100,
+			MinimumTemperature: &minimumTemperature,
+		})
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "warehouse_code")
+	})
+
+	t.Run("should return error if address is empty", func(t *testing.T) {
+		err := validateWarehouseAttributes(models.Warehouse{
+			WarehouseCode:      "WH-1",
+			Address:            "   ",
+			Telephone:          "1234567890",
+			MinimumCapacity:    100,
+			MinimumTemperature: &minimumTemperature,
+		})
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "address")
+	})
+
+	t.Run("should return error if telephone is empty", func(t *testing.T) {
+		err := validateWarehouseAttributes(models.Warehouse{
+			WarehouseCode:      "WH-1",
+			Address:            "Street 1",
+			Telephone:          "",
+			MinimumCapacity:    100,
+			MinimumTemperature: &minimumTemperature,
+		})
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "telephone")
+	})
+
+	t.Run("should return error if minimum capacity is negative", func(t *testing.T) {
+		err := validateWarehouseAttributes(models.Warehouse{
+			WarehouseCode:      "WH-1",
+			Address:            "Street 1",
+			Telephone:          "1234567890",
+			MinimumCapacity:    -100,
+			MinimumTemperature: &minimumTemperature,
+		})
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "minimun_capacity")
+	})
+
+	t.Run("should return error if minimum temperature is nil", func(t *testing.T) {
+		err := validateWarehouseAttributes(models.Warehouse{
+			WarehouseCode:      "WH-1",
+			Address:            "Street 1",
+			Telephone:          "1234567890",
+			MinimumCapacity:    100,
+			MinimumTemperature: nil,
+		})
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "minimun_temperature")
+	})
+
+	t.Run("should return nil if all fields are valid", func(t *testing.T) {
+		err := validateWarehouseAttributes(models.Warehouse{
+			WarehouseCode:      "WH-1",
+			Address:            "Street 1",
+			Telephone:          "1234567890",
+			MinimumCapacity:    100,
+			MinimumTemperature: &minimumTemperature,
+		})
+		require.NoError(t, err)
 	})
 }
