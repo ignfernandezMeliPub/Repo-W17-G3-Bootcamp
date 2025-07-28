de897c591504c606d493330da6e5725b24cb57fc
lmagri <ludmila.magri@mercadolibre.com>
2025-07-28 10:37:05 -0300

    test en handler de carrie y en service de warehouse

--- internal/service/warehouse_test.go
+++ internal/service/warehouse_test.go
@@ -10,15 +10,15 @@ import (
 )
 
 func Test_UpdateWarehouseById(t *testing.T) {
-	warehouse := models.Warehouse{
-		Id:                 1,
-		WarehouseCode:      "W001",
-		Address:            "Street 123",
-		Telephone:          "1234567890",
-		MinimumCapacity:    10,
-		MinimumTemperature: &[]float64{10.0}[0],
-	}
 	t.Run("should update warehouse successfully", func(t *testing.T) {
+		warehouse := models.Warehouse{
+			Id:                 1,
+			WarehouseCode:      "W001",
+			Address:            "Street 123",
+			Telephone:          "1234567890",
+			MinimumCapacity:    10,
+			MinimumTemperature: &[]float64{10.0}[0],
+		}
 
 		mockRepository := new(repository.MockWarehouseRepository)
 		mockRepository.On("GetWarehouseById", 1).Return(warehouse, nil)
@@ -34,6 +34,15 @@ func Test_UpdateWarehouseById(t *testing.T) {
 	})
 
 	t.Run("should return error if warehouse does not exist", func(t *testing.T) {
+		warehouse := models.Warehouse{
+			Id:                 1,
+			WarehouseCode:      "W001",
+			Address:            "Street 123",
+			Telephone:          "1234567890",
+			MinimumCapacity:    10,
+			MinimumTemperature: &[]float64{10.0}[0],
+		}
+
 		mockRepository := new(repository.MockWarehouseRepository)
 		mockRepository.On("GetWarehouseById", 1).Return(models.Warehouse{}, custom_errors.ErrNotFound)
 
@@ -48,9 +57,18 @@ func Test_UpdateWarehouseById(t *testing.T) {
 	})
 
 	t.Run("should return error if minimum capacity is less than 0", func(t *testing.T) {
-		warehouse.MinimumCapacity = -1
+		warehouse := models.Warehouse{
+			Id:                 1,
+			WarehouseCode:      "W001",
+			Address:            "Street 123",
+			Telephone:          "1234567890",
+			MinimumCapacity:    -1,
+			MinimumTemperature: &[]float64{10.0}[0],
+		}
+
 		mockRepository := new(repository.MockWarehouseRepository)
 		mockRepository.On("GetWarehouseById", 1).Return(warehouse, nil)
+
 		service := NewWarehouseDefault(mockRepository)
 
 		_, err := service.UpdateWarehouseById(1, warehouse)
