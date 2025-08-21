58e7afcaaddae80d4cf6dff707e445f934da532d
ignfernandez_meli <ignacioagustin.fernandez@mercadolibre.com>
2025-08-21 17:31:13 -0300

    fix: MySQL Error mapping fix

--- internal/repository/sql_utils/helpers.go
+++ internal/repository/sql_utils/helpers.go
@@ -228,6 +228,30 @@ func HandleSqlError(err error) error {
 			IsParentRow:    false,
 			Details:        mysqlErr.Message,
 		}
+	case 1216: // Cannot delete or update a parent row: a foreign key constraint fails
+		var fkConstraintRegex = regexp.MustCompile("FOREIGN KEY \\(`([^`]*)`\\)")
+		matches := fkConstraintRegex.FindStringSubmatch(mysqlErr.Message)
+		constraintName := ""
+		if len(matches) >= 2 {
+			constraintName = matches[1]
+		}
+		return &custom_errors.ForeignKeyViolationError{
+			ConstraintName: constraintName,
+			IsParentRow:    true,
+			Details:        mysqlErr.Message,
+		}
+	case 1217: // Cannot delete or update a child or parent row: a foreign key constraint fails
+		var fkConstraintRegex = regexp.MustCompile("FOREIGN KEY \\(`([^`]*)`\\)")
+		matches := fkConstraintRegex.FindStringSubmatch(mysqlErr.Message)
+		constraintName := ""
+		if len(matches) >= 2 {
+			constraintName = matches[1]
+		}
+		return &custom_errors.ForeignKeyViolationError{
+			ConstraintName: constraintName,
+			IsParentRow:    false,
+			Details:        mysqlErr.Message,
+		}
 	}
 
 	return err
